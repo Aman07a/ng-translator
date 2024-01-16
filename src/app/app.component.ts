3a00c0faa4bc060bc1b235b5a2ce5f990aa62335
Aman07a <aman16.aa17@gmail.com>
2024-01-16 19:17:33 +0100

    Intro - Our First App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslateService } from './translate.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-translator';
+  constructor(private translateService: TranslateService) {}
 }
